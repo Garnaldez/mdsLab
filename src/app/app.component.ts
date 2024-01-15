679680b05ad3ad29ca1fee949298699dc593346f
Garnaldez <garnaldez@gmail.com>
2024-01-15 19:17:02 +0100

    href

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { APP_BASE_HREF } from '@angular/common';
-import { Component, NgModule, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-})
-
-@NgModule({
-  providers: [{provide: APP_BASE_HREF, useValue: '/mdsLab/'}]
 })
 export class AppComponent {
   showMenu: boolean = false;
